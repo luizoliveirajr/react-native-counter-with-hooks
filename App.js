7d3648b4007664fe8327e499030cf6c701c9785d
BigMachine1 <luizmjunior@hotmail.com>
2020-12-10 11:59:13 -0300

    count implement

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  const increment = () =>{
+    setCount(count + 1);
+  };
+
+  const reset = () =>{
+    setCount(0);
+  };
+
+  const decrement = () =>{
+    setCount(count -1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 60}}>Counter</Text>
+      <Text style={{fontSize: 60}}>{count}</Text>
+      <View style={[{width: 300, margin: 10}]}>
+        <Button color="green" title="Increment" onPress={increment} />
+      </View>
+      <View style={[{width: 300, margin: 10}]}>
+        <Button title="Reset" onPress={reset} />
+      </View>
+      <View style={[{width: 300, margin: 10}]}>
+        <Button color="red" title="Decrement" onPress={decrement} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
